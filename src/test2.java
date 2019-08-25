78760bcc7809da3f4ba187241918670afe9a76c9
Aswathy Sali <aswathysali1994@gmail.com>
2019-08-25 21:10:06 +0530

    my new change!!!

--- src/test2.java
+++ src/test2.java
@@ -4,6 +4,7 @@ public class test2 {
 	public static void main(String[] args) {
 		System.out.println("first message");
 		System.out.println("changing the second class");
+		System.out.println("my new chnage");
 
 	}
 
